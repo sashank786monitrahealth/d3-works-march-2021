a09a66af65c3b21b616772a897ae4b3065b06899
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-03-05 08:25:19 +0530

    selecting first element in 3 ways

--- 010-svg-select/select.js
+++ 010-svg-select/select.js
@@ -1,2 +1,11 @@
 const h1 = d3.select("h1")
 console.log(h1);
+
+let firstSVG = d3.select("svg");
+console.log(firstSVG);
+
+firstSVG = d3.select("svg#circles");
+console.log(firstSVG);
+
+firstSVG = d3.select("#circles");
+console.log(firstSVG);
