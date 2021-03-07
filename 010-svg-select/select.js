1e5a158acaa155ede4119928c86b1e08584bcdeb
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-03-07 11:18:28 +0530

    edit

--- 010-svg-select/select.js
+++ 010-svg-select/select.js
@@ -9,3 +9,32 @@ console.log(firstSVG);
 
 firstSVG = d3.select("#circles");
 console.log(firstSVG);
+
+
+let secondSvg = d3.select("#rects");
+console.log(secondSvg);
+
+secondSvg = d3.select('svg:nth-of-type(2)');
+console.log(secondSvg);
+
+
+let firstCircle = d3.select('circle');
+console.log(firstCircle);
+
+let secondCircle = d3.select('circle:nth-of-type(2)');
+console.log(secondCircle);
+
+let firstRect = d3.select('rect');
+console.log(firstRect);
+
+let secondRect = d3.select('rect:nth-of-type(2)');
+console.log(secondRect);
+
+firstRect = secondSvg.select('rect');
+console.log(firstRect);
+
+   console.log(d);
+   console.log(i);
+   console.log(n);
+})
+
