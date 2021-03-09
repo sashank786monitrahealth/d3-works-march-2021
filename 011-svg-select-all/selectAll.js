7c9d9bed653c93cf3769177a71554fb52f8ab384
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-03-09 20:47:49 +0530

    allRects and allCircles

--- 011-svg-select-all/selectAll.js
+++ 011-svg-select-all/selectAll.js
@@ -3,3 +3,10 @@ console.log(h1SA);
 
 const h1S = d3.select("h1");
 console.log(h1S);
+
+
+let allCircles = d3.selectAll("circle");
+console.log(allCircles);
+
+let allRects = d3.selectAll("rect");
+console.log(allRects);
