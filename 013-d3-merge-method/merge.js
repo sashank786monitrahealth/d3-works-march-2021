b5805bac987c911bb294e8f494ed7fb26254ce3c
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-03-10 16:18:30 +0530

    init

---
+++ 013-d3-merge-method/merge.js
@@ -0,0 +1,28 @@
+let allCircles = d3.selectAll("circle");
+console.log(allCircles);
+
+let allCirclesOdd = allCircles.filter(':nth-child(odd)');
+console.log(allCirclesOdd);
+
+
+let allCirclesEven = allCircles.filter(':nth-child(even)');
+console.log(allCirclesEven);
+
+console.log("odd class");
+allCirclesOdd = allCircles.filter('.odd');
+console.log(allCirclesOdd);
+
+
+console.log("even class");
+allCirclesEven = allCircles.filter('.even');
+console.log(allCirclesEven);
+
+allCircles.filter(function(d,i,n){
+  console.log(d);
+  console.log(i);
+  console.log(n);
+  console.log(n[i]);
+  console.log(this);
+  //this.style.fill = "dodgerblue";
+  i%2==0?this.style.fill="green":this.style.fill="dodgerblue";
+})
