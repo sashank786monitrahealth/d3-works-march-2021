d96741ab4e949e9324794988ff5336f7fef98b68
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-03-11 21:13:45 +0530

    allCirclesOddEven

--- 013-d3-merge-method/merge.js
+++ 013-d3-merge-method/merge.js
@@ -6,10 +6,58 @@ console.log(allCSvg1);
 let allCSvg2 = d3.selectAll("#svg2 circle");
 console.log(allCSvg2);
 
-allCSvg1.select(function(d,i,n){
+let allCSvg1Odd = allCSvg1.select(function(d,i,n){
     console.log(d);
     console.log(i);
     console.log(n[i]);
-    console.log(this);
-    this.style.fill = 'indianred';
+    
+    if (i%2==0){
+    
+       //console.log(this);
+       this.style.fill = 'indianred';
+       return this;
+    
+    } else {
+    
+       return null;
+    
+    }
+    
+    
+    
 });
+
+
+console.log(allCSvg1Odd);
+
+
+
+let allCSvg1Even = allCSvg1.select(function(d,i,n){
+    console.log(d);
+    console.log(i);
+    console.log(n[i]);
+    
+    if (i%2==0){
+    
+             return null;
+    
+    } else {
+    
+           //console.log(this);
+       this.style.fill = 'lightgreen';
+       return this;
+    
+
+    
+    }
+    
+    
+    
+});
+
+
+console.log(allCSvg1Even);
+
+let allCirclesOddEven = allCSvg1Odd.merge(allCSvg1Even);
+console.log(allCirclesOddEven);
+
