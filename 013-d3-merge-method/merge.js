5ef4709a94074b518248f72b460bf5c25542aef7
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-03-11 20:42:18 +0530

    change circle color to indianred

--- 013-d3-merge-method/merge.js
+++ 013-d3-merge-method/merge.js
@@ -5,3 +5,11 @@ console.log(allCSvg1);
 
 let allCSvg2 = d3.selectAll("#svg2 circle");
 console.log(allCSvg2);
+
+allCSvg1.select(function(d,i,n){
+    console.log(d);
+    console.log(i);
+    console.log(n[i]);
+    console.log(this);
+    this.style.fill = 'indianred';
+});
