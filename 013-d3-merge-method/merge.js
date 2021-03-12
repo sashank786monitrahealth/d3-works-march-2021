6bb7ae81bf1406f2b6e57fa307fd525a3c6613b8
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-03-12 07:48:28 +0530

    work on SVG2 - second group of circles

--- 013-d3-merge-method/merge.js
+++ 013-d3-merge-method/merge.js
@@ -3,8 +3,8 @@
 let allCSvg1 = d3.selectAll("#svg1 circle");
 console.log(allCSvg1);
 
-let allCSvg2 = d3.selectAll("#svg2 circle");
-console.log(allCSvg2);
+//let allCSvg2 = d3.selectAll("#svg2 circle");
+//console.log(allCSvg2);
 
 let allCSvg1Odd = allCSvg1.select(function(d,i,n){
     console.log(d);
@@ -62,8 +62,83 @@ let allCirclesOddEven = allCSvg1Odd.merge(allCSvg1Even);
 console.log(allCirclesOddEven);
 
 
+
+
+
+/*##############################################
+### BEGIN: SVG2
+##############################################*/
+
+let allCSvg2 = d3.selectAll("#svg2 circle");
+console.log(allCSvg2);
+
+
+
 let allCSvg = allCSvg1.merge(allCSvg2);
 console.log(allCSvg);
 
+let allCSvg2Odd = allCSvg2.select(function(d,i,n){
+    console.log(d);
+    console.log(i);
+    console.log(n[i]);
+    
+    if (i%2==0){
+    
+       //console.log(this);
+       this.style.fill = 'dodgerblue';
+       return this;
+    
+    } else {
+    
+       return null;
+    
+    }
+    
+    
+    
+});
 
 
+console.log(allCSvg2Odd);
+
+
+
+let allCSvg2Even = allCSvg2.select(function(d,i,n){
+    console.log(d);
+    console.log(i);
+    console.log(n[i]);
+    
+    if (i%2==0){
+    
+             return null;
+    
+    } else {
+    
+           //console.log(this);
+       this.style.fill = 'emeraldgreen';
+       return this;
+    
+
+    
+    }
+    
+    
+    
+});
+
+
+console.log(allCSvg2Even);
+
+//let allCirclesOddEven = allCSvg2Odd.merge(allCSvg2Even);
+//console.log(allCirclesOddEven);
+
+
+//let allCSvg = allCSvg2.merge(allCSvg2);
+//console.log(allCSvg);
+
+
+
+
+/*##############################################
+### END: SVG2
+##############################################*/
