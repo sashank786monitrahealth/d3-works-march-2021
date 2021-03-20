1c15063da5016a77443e668353d71ff81e71dce1
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-03-20 18:34:53 +0530

    selectAll child

--- 014-d3-select-child-method/selectChild.js
+++ 014-d3-select-child-method/selectChild.js
@@ -15,3 +15,25 @@ console.log(firstRect);
 let secondRect = d3.select("svg:nth-of-type(2)").selectChild(":nth-of-type(2)");
 console.log(secondRect);
 
+
+let firstChild = d3.selectAll("svg").selectChild();
+console.log(firstChild);
+
+let secondChild = d3.selectAll("svg").selectChild(":nth-of-type(2)");
+console.log(secondChild);
+
+
+d3.select("svg").selectChild(function(c,i,cn){
+   console.log(c);
+   console.log(i);
+   console.log(cn);
+});
+
+
+d3.selectAll("svg").selectChild(function(c,i,cn){
+   console.log(c);
+   console.log(i);
+   console.log(cn);
+})
+
+
