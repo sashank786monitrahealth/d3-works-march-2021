a5c81eb13ab88377e4e5814f4563e9e75c9e528e
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-04-04 15:09:52 +0530

    using classed method

--- 018-method-classed/classed_method.js
+++ 018-method-classed/classed_method.js
@@ -12,3 +12,24 @@ line1 = d3.select("line")
 */          
 console.log(d3.select("line")
           .classed("line1"));
+          
+          
+
+lines = d3.selectAll("line")
+          .classed("orange",true);
+          
+console.log(lines);
+
+ d3.selectAll("line")
+          .classed("orange",false);
+          
+//remove class attribute
+d3.selectAll("line")
+  .attr("class",null);
+  
+lines.classed('dasharray', function(d,i,n){
+     console.log(d);
+     console.log(i);
+     console.log(n);
+     return i%2==0?true:false;
+})
