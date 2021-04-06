c4a86e9e6c045694deaa70facbbc48f4440c2732
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-04-06 17:46:24 +0530

    add style to ellipses

--- 019-method-style/style_method.js
+++ 019-method-style/style_method.js
@@ -7,4 +7,29 @@ console.log(svg.style("background-color"));
 svg.attr('height','300');
 svg.attr('width','600');
 
-svg.style('background-color','lightgray');
+let svgColor = svg.style('background-color','lightgray');
+console.log(svg.style('background-color'));
+console.log(svgColor);
+
+svg.style('background-color',null);
+
+let ellipses = svg.selectChildren('ellipse');
+console.log(ellipses);
+
+ellipses.select(function(d,i){
+     //d3.select(this);
+     console.log("this",this);
+     const ellipse = d3.select(this);
+     console.log(ellipse);
+     ellipse.attr('cx',`${i*100+110}`)
+            .attr('cy',`${i*80+80}`)
+            .attr('rx',`${i*20+50}`)
+            .attr('ry',`${i*20+70}`);
+            
+     //ellipse.style("fill",`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`);
+});
+
+ellipses.style("fill",function(d,i){
+   return `rgb(${Math.random()*120},${Math.random()*120},${Math.random()*120})`;
+}
+);
