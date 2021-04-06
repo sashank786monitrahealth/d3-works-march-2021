8b3ff29d1ea09cd89cd1f5653aa174b027eb7503
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-04-06 14:50:48 +0530

    svg background lightgray

--- 019-method-style/style_method.js
+++ 019-method-style/style_method.js
@@ -1,1 +1,10 @@
 //selection.style(name,value)
+
+let svg = d3.select("svg");
+console.log(svg.style("background-color"));
+
+
+svg.attr('height','300');
+svg.attr('width','600');
+
+svg.style('background-color','lightgray');
