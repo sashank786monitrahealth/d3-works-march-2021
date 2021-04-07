f9941054961736addfeb44db407f1f1035e86210
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-04-07 15:08:20 +0530

    use property method

--- 020-method-property/property_method.js
+++ 020-method-property/property_method.js
@@ -1,35 +1,25 @@
 //selection.style(name,value)
 
-let svg = d3.select("svg");
-console.log(svg.style("background-color"));
+const labelsFor = ['fname','lname','gender','male','female','nomention'];
+const labels = d3.selectAll('label');
 
+console.log(labels);
 
-svg.attr('height','300');
-svg.attr('width','600');
-
-let svgColor = svg.style('background-color','lightgray');
-console.log(svg.style('background-color'));
-console.log(svgColor);
-
-svg.style('background-color',null);
-
-let ellipses = svg.selectChildren('ellipse');
-console.log(ellipses);
-
-ellipses.select(function(d,i){
-     //d3.select(this);
-     console.log("this",this);
-     const ellipse = d3.select(this);
-     console.log(ellipse);
-     ellipse.attr('cx',`${i*100+110}`)
-            .attr('cy',`${i*80+80}`)
-            .attr('rx',`${i*20+50}`)
-            .attr('ry',`${i*20+70}`);
-            
-     //ellipse.style("fill",`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`);
+labels.filter(function(d,i){
+    const d3label = d3.select(this);
+    d3label.attr('for',labelsFor[i]);
+    
 });
 
-ellipses.style("fill",function(d,i){
-   return `rgb(${Math.random()*120},${Math.random()*120},${Math.random()*120})`;
-}
-);
+d3.select('#fname')
+  .property('type','text')
+  .property('placeholder','Praneeth');
+  
+console.log(d3.select('#fname').property('placeholder'));
+
+d3.select('#lname')
+  .property('type','text')
+  .property('placeholder','Gone');
+  
+console.log(d3.select('#fname').property('type'));
+  
