fa176346083b7a0add9ced847c73a439b73516bb
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-04-11 13:16:08 +0530

    init

---
+++ 023-method-html/method_html.js
@@ -0,0 +1,7 @@
+// selection.html(typel)
+
+/*
+ html method is supported only on HTML elements. It is not supported on other non-html elements - like SVG elements. Because, they do not have the innerHTML property.
+*/
+
+
