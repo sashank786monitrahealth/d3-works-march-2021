c00b28f9c482033d2f2bef0c0df4711cacaf8ae6
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-04-11 14:46:51 +0530

    console log h1

--- 023-method-html/method_html.js
+++ 023-method-html/method_html.js
@@ -4,4 +4,14 @@
  html method is supported only on HTML elements. It is not supported on other non-html elements - like SVG elements. Because, they do not have the innerHTML property.
 */
 
-
+let h1 = d3.select('h1')
+           .html();
+           
+   console.log("h1=")
+   console.log(h1);
+   
+ 
+ d3.select('h1')
+           .html("HTML");
+   console.log(h1);
+   
