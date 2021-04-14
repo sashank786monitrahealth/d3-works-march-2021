38f180a02bd6d2993f07d3f464c351309cda6a9f
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-04-14 07:09:51 +0530

    insert

--- 024-method-insert/method_insert.js
+++ 024-method-insert/method_insert.js
@@ -5,6 +5,26 @@ let svg1 = d3.select("#svg1");
 let text1 = svg1.insert("text")
                 .text("inserted with text method")
                 .attr("x","5")
-                .attr("y","20");
+                .attr("y","40");
                 
 console.log(text1);
+
+
+
+
+let text2 = svg1.insert("text")
+                .text("inserted with text method - using before selector")
+                .attr("x","5")
+                .attr("y","20");
+                
+console.log(text2);
+
+
+
+
+let text3 = svg1.insert("text")
+                .text("inserted with text method - as the last child")
+                .attr("x","5")
+                .attr("y","60");
+                
+console.log(text3);
