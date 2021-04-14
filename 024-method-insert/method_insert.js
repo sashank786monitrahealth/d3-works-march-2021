791b20a86cf94b6bc00d6a7d9f06aecaf430af9a
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-04-14 07:25:51 +0530

    this.firstElementChild

--- 024-method-insert/method_insert.js
+++ 024-method-insert/method_insert.js
@@ -28,3 +28,22 @@ let text3 = svg1.insert("text")
                 .attr("y","60");
                 
 console.log(text3);
+
+// now we are going to find - how to pass in functions instead of selectors
+
+let svg2 = d3.select("#svg2");
+
+for (i=0;i<2;i++){
+   svg2.insert(function(){
+      return document.createElementNS("http://www.w3.org/2000/svg","text")
+      // call other function to specify the before selector
+   }, function(){
+        console.log(this.firstElementChild);
+      return this.firstElementChild;
+   })
+     .text(`${i}`)
+     .attr('x','5')
+     .attr('y',`${i*20+20}`)
+};
+
+
