4f231d04cc6404aea973b7bdbd186c51d5098b5e
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-04-17 07:14:35 +0530

    remove rect

--- 025-method-remove/method_remove.js
+++ 025-method-remove/method_remove.js
@@ -6,4 +6,7 @@ console.log(circle);
 let circleRemoved = circle.remove();
 console.log(circleRemoved);
 
-               
+let rect = d3.select("rect");
+console.log(rect)       ;
+let rectRemoved = rect.remove();
+console.log(rectRemoved);
