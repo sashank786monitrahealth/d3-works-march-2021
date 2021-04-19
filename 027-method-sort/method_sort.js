3d8607114b17e8f6c07ea024f4f36c717848a785
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-04-19 07:08:10 +0530

    add 5 elements

--- 027-method-sort/method_sort.js
+++ 027-method-sort/method_sort.js
@@ -1,13 +1,6 @@
-//selection.clone(deep)
+//selection.sort(compare)
+/*This returns a selection that returns each group in the selection - sorted according to the compare function. After sorting, it will insert elements as per the resulting order. Now the compare function which defaults to ascending is passed to elements data
 
-let svg = d3.select('svg');
-let svgClone = svg.clone();
-console.log(svgClone);
+Sort only repositions the elements in the dom. The elements need to be repositioned on the screen on their new order in the dom and the attr method will be called after sort. 
+*/
 
-
-
-
-
-
-svgClone = svg.clone(true);
-console.log(svgClone);
