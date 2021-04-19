261000ee6d03fd922ab5e8ce55f0bb357eb6e6b3
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-04-19 07:34:07 +0530

    sort paragraph elements desc asc

--- 027-method-sort/method_sort.js
+++ 027-method-sort/method_sort.js
@@ -4,3 +4,18 @@
 Sort only repositions the elements in the dom. The elements need to be repositioned on the screen on their new order in the dom and the attr method will be called after sort. 
 */
 
+
+d3.selectAll('p')
+  .datum(function(){
+     return this.innerText
+  });
+  
+console.log(d3.selectAll("p"));
+
+
+d3.selectAll('p')
+  .datum(function(){
+     return this.innerText
+  })
+  .sort((a,b) => b-a)  // descending
+  .sort((a,b) => a-b); // ascending
