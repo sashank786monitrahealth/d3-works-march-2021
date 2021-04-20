00ce4b40d973641743349d28b4ff5c9a61af12cf
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-04-20 06:36:52 +0530

    indianread set as first element to be drawn

--- 029-method-lower/method-lower.js
+++ 029-method-lower/method-lower.js
@@ -1,3 +1,5 @@
 //selection.lower()
 
-d3.select('rect:nth-of-type(3)').lower();
+d3.select('rect:nth-of-type(3)').lower(); //mediumseagreen
+
+d3.select('rect:nth-of-type(2)').lower(); // indianred
