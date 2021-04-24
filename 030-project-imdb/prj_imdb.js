a5ccd9106c8fb44cce78b8065ad5c96a5bbe0bf0
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2021-04-24 09:35:16 +0530

    progress till 8:29

---
+++ 030-project-imdb/prj_imdb.js
@@ -0,0 +1,82 @@
+const movieData = [
+  {
+    name: 'Avengers: Endgame',
+    contentRating: '12A',
+    duration: 181, // in minutes
+    starRating: 8.4, // out of 10
+    votes: 747374,
+    gross: 858 // USD million
+  },
+  {
+    name: 'The Lion King',
+    contentRating: 'PG',
+    duration: 118,
+    starRating: 6.9,
+    votes: 198014,
+    gross: 544
+  },
+  {
+    name: 'Star Wars: The Rise of Skywalker',
+    contentRating: '12A',
+    duration: 141,
+    starRating: 6.6,
+    votes: 343828,
+    gross: 515
+  },
+  {
+    name: 'Frozen 2',
+    contentRating: 'U',
+    duration: 103,
+    starRating: 6.9,
+    votes: 120859,
+    gross: 477
+  },
+  {
+    name: 'Toy Story 4',
+    contentRating: 'U',
+    duration: 100,
+    starRating: 7.8,
+    votes: 187391,
+    gross: 434
+  },
+  {
+    name: 'Captain Marvel',
+    contentRating: '12A',
+    duration: 123,
+    starRating: 6.9,
+    votes: 420459,
+    gross: 427
+  },
+  {
+    name: 'Spider-Man: Far From Home',
+    contentRating: '12A',
+    duration: 129,
+    starRating: 7.5,
+    votes: 301963,
+    gross: 391
+  },
+  {
+    name: 'Aladdin',
+    contentRating: 'PG',
+    duration: 128,
+    starRating: 7.0,
+    votes: 213479,
+    gross: 356
+  },
+  {
+    name: 'Joker',
+    contentRating: '15',
+    duration: 122,
+    starRating: 8.5,
+    votes: 840556,
+    gross: 335
+  },
+  {
+    name: 'Jumanji: The Next Level',
+    contentRating: '12A',
+    duration: 123,
+    starRating: 6.7,
+    votes: 163288,
+    gross: 317
+  },
+];
